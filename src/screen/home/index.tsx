1a1683dec0ee3554202b7d5ca5b80530846603e5
Bhadri2015-SB <bhadri2919@gmail.com>
2025-09-03 17:44:21 +0530

    functional update

--- src/screen/home/index.tsx
+++ src/screen/home/index.tsx
@@ -5,13 +5,20 @@ import generative from "@/assets/images/generative.png";
 import approve from "@/assets/images/approve.png";
 import { Textarea } from "@/components/ui/textarea";
 import reject from "@/assets/images/decline.png";
+import axios from "axios";
+import { successToast } from "@/components/Toaster";
+import Markdown from 'react-markdown'
 
 const HomePage = () => {
 
     const [inputValue, setInputValue] = useState('');
     const [validErr, setValidErr] = useState('');
     const [rejectModal, setRejectModal] = useState(false);
-    const [testModal, setTestModal] = useState(false);
+    // const [testModal, setTestModal] = useState(false);
+    const [ThreadId, setThreadId] = useState<any>();
+    const [loading, setLoading] = useState(false);
+    const [Response, setResponse] = useState<any>();
+    const [feedback, setFeedback] = useState<any>('');
 
     const handleActualChange = (value: string) => {
 		if (value === '' || /^\d*\.?\d{0,2}$/.test(value)) {
@@ -28,28 +35,81 @@ const HomePage = () => {
             setValidErr('Please enter a number');
             return;
         } 
-        setTestModal(true);
+        // setTestModal(true);
         console.log(inputValue);
+        const thread_id = Math.floor(Math.random() * 100) + 1;
+        setThreadId(thread_id);
+        setLoading(true);
+        const body = {
+            "query": `analyze and generate budget recommendation for ${inputValue} days`,
+            "feedback": "",
+            "thread_id": thread_id
+        }
+        axios.post('http://127.0.0.1:8000/agent', body).then((res) => {
+            console.log(res.data);
+            setResponse(res.data);
+            setLoading(false);
+        }).catch((err) => {
+            console.error(err);
+            setLoading(false);
+        });
     }
 
     const handleApprove = () => {
+        setLoading(true);
         console.log('Approved');
+        const body = {
+            "query": `approve`,
+            "feedback": "",
+            "thread_id": ThreadId
+        }
+        axios.post('http://127.0.0.1:8000/agent', body).then((res) => {
+            console.log(res.data);
+            
+            if(res.data.result){
+                successToast('Mail has been sent to the manager');
+                setResponse(res.data);
+            }
+            setLoading(false);
+        }).catch((err) => {
+            console.error(err);
+            setLoading(false);
+        });
     }
 
     const handleReject = () => {
         setRejectModal(true);
+        setLoading(true);
+        const body = {
+            "query": `rejected`,
+            "feedback": feedback,
+            "thread_id": ThreadId
+        }
+        axios.post('http://127.0.0.1:8000/agent', body).then((res) => {
+            console.log(res.data);
+            if(res.data.result){
+                // successToast('Mail has been sent to the manager');
+                setResponse(res.data);
+            }
+            
+            setLoading(false);
+        }).catch((err) => {
+            console.error(err);
+            setLoading(false);
+        });
     }
 
     // const randomNumber = Math.floor(Math.random() * 100) + 1;
   return (
 
-    // {(loading || filterLoading || magicRosterLoading) && (
-	// 			<div className="absolute inset-0 z-10 bg-gray-200/50 flex items-center justify-center">
-	// 				<div className="loader"></div>
-	// 			</div>
-	// 		)}
+    
     <div className="py-4 px-2 mt-3">
-        <div className={`grid ${testModal ? 'grid-cols-2' : 'grid-cols-1'} gap-5`}>
+    {(loading) && (
+				<div className="absolute inset-0 z-10 bg-gray-200/50 flex items-center justify-center">
+					<div className="loader"></div>
+				</div>
+			)}
+        <div className={`grid ${Response?.result  ? 'grid-cols-2' : 'grid-cols-1'} gap-5`}>
             <div>
                 <div className="bg-white border border-gray-200 rounded-md py-6 md:px-9 px-2 h-fit">
                     <div className="text-sm font-medium">How many days you want to analyze the campaigns</div>
@@ -74,7 +134,7 @@ const HomePage = () => {
                 </div>
 
                 {
-                    testModal && (
+                    Response?.result && (
                         <div>
                             <div className="bg-white border border-gray-200 rounded-md py-6 md:px-9 px-2 h-fit mt-5">
                                 <div className="">
@@ -98,7 +158,7 @@ const HomePage = () => {
                                         <div className="">
                                             <div className="text-sm font-medium mb-3">Reason for rejection</div>
                                             <div>
-                                                <Textarea placeholder="Type your message here." />
+                                                <Textarea placeholder="Type your message here." onChange={(e) => setFeedback(e.target.value)} value={feedback}  />
                                             </div>
                                             <div className="mt-3 flex justify-end">
                                                 <Button variant="outline" onClick={() => setRejectModal(false)} className="cursor-pointer mr-2">Cancel</Button>
@@ -115,12 +175,12 @@ const HomePage = () => {
             </div>
 
             {
-                testModal && (
+                Response?.result && (
                     <div className="bg-white border border-gray-200 rounded-md py-6 md:px-9 px-2">
                         <div className="">
                             <div className="text-sm font-medium mb-3">Agent Response</div>
                             <div className="h-[400px] overflow-y-scroll">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias minus ut, unde nemo sint ad totam, officiis rem eligendi sunt natus. Dolorum sequi odit sint, numquam nobis rem aperiam excepturi reiciendis officiis, non odio quo. Corporis quam iusto fugiat quae dicta earum sequi debitis, et optio repellat cumque consequatur recusandae error tempora odio omnis consectetur sed, ex totam natus a aspernatur. Modi deleniti facilis numquam maiores velit magnam iusto! Non sapiente delectus dicta sequi eum exercitationem, voluptas enim, libero ex quibusdam porro hic sed nihil quos architecto iusto ipsa quasi asperiores excepturi saepe! Tempora sequi id consequatur eaque saepe sint vitae! Temporibus rem nisi praesentium adipisci cum fuga eius deleniti deserunt, numquam dolorem, repellat doloremque iusto maxime dignissimos at unde sit cumque soluta nostrum aperiam. Dolorem iure in, esse doloremque dolore et iusto architecto non possimus aspernatur tempora, laborum porro repudiandae? Ut sunt sit, aperiam dignissimos modi quaerat quae? Aliquid quasi, consectetur itaque exercitationem mollitia similique sapiente recusandae ab rem adipisci odio officiis voluptatum, tempore earum quia distinctio sequi dolores hic quibusdam? Eius accusamus voluptatum optio ad, magni laborum temporibus dolorum maxime suscipit repellat dicta deleniti tempora, ullam molestiae unde tempore itaque. Optio delectus nihil ipsa! Dicta ratione explicabo maxime numquam vitae illum beatae odio quasi fuga, ex nesciunt, rerum cumque nisi esse. Aut debitis commodi ipsam ipsum nemo ex blanditiis non. Eaque ad voluptatem quae nulla! Odit, rem impedit nisi accusamus excepturi architecto fuga molestias unde. Eius voluptatem quaerat dolor asperiores pariatur cumque nemo atque necessitatibus, quidem temporibus quibusdam, quas deserunt fugiat harum incidunt nostrum ab saepe fuga nesciunt deleniti laborum illo ullam inventore. Aut qui, vero possimus veritatis modi aspernatur, fugiat porro laudantium maiores nisi, voluptatibus iusto nam sint earum perferendis minima mollitia. Esse dolorum quas modi dolorem, quibusdam doloribus odit, nihil provident veniam, neque exercitationem quo! Ad.
+                                <Markdown>{Response.result}</Markdown>
                             </div>
                         </div>
 
